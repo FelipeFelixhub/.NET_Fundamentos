aff49630967c8c23c9780a23ecfb30f2539b4a7f
Felipe Felix <felipe.dfelix@gmail.com>
2023-07-11 18:26:22 -0300

    Formatando DateTime

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,18 +1,30 @@
 ﻿using ExemploExplorando.Models;
 using System.Globalization;
 
-CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
 
-decimal valorMonetario = 2482.40M;
+DateTime data = DateTime.Now;
+System.Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
 
-System.Console.WriteLine(valorMonetario.ToString("C4"));
 
-double porcentagem = .4545;
 
-System.Console.WriteLine(porcentagem.ToString("P"));
 
-int numero = 123456;
-System.Console.WriteLine(numero.ToString("###-###"));
+
+
+
+
+
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+// decimal valorMonetario = 2482.40M;
+
+// System.Console.WriteLine(valorMonetario.ToString("C4"));
+
+// double porcentagem = .4545;
+
+// System.Console.WriteLine(porcentagem.ToString("P"));
+
+// int numero = 123456;
+// System.Console.WriteLine(numero.ToString("###-###"));
 
 
 
