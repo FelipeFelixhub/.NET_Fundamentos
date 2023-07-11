7b06ee5a7647e8106031a3a079087f4ececb5cd8
Felipe Felix <felipe.dfelix@gmail.com>
2023-07-11 16:23:30 -0300

    Formatando moeda

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -1,14 +1,27 @@
 ﻿using ExemploExplorando.Models;
+using System.Globalization;
 
-Pessoa p1 = new Pessoa(nome:"Felipe", sobrenome:"Felix");
-Pessoa p2 = new Pessoa(nome:"Henrique",sobrenome:"Souza Felix");
-Pessoa p3 = new Pessoa(nome:"Caroline",sobrenome: "Felix");
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Ingles";
-cursoDeIngles.Alunos = new List<Pessoa>();
+decimal valorMonetario = 2482.40M;
 
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.AdicionarAluno(p3);
-cursoDeIngles.ListarAlunos();
+System.Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa(nome:"Felipe", sobrenome:"Felix");
+// Pessoa p2 = new Pessoa(nome:"Henrique",sobrenome:"Souza Felix");
+// Pessoa p3 = new Pessoa(nome:"Caroline",sobrenome: "Felix");
+
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Ingles";
+// cursoDeIngles.Alunos = new List<Pessoa>();
+
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.AdicionarAluno(p3);
+// cursoDeIngles.ListarAlunos();
