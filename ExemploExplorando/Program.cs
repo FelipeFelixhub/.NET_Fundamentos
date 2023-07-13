6040610736f32255d95355582c6bb2a98a3cd28d
Felipe Felix <felipe.dfelix@gmail.com>
2023-07-13 20:07:38 -0300

    DateTime em Json

--- ExemploExplorando/Program.cs
+++ ExemploExplorando/Program.cs
@@ -4,10 +4,13 @@ using Newtonsoft.Json;
 
 //SERIALIZACAO
 
+DateTime dataAtual = DateTime.Now;
+
+
 List<Venda> listaVendas = new List<Venda>();
 
-Venda v1 = new Venda(1, "Material de escritorio", 25.00M);
-Venda v2 = new Venda(2,"Licenca de Software", 130.00M);
+Venda v1 = new Venda(1, "Material de escritorio", 25.00M, dataAtual);
+Venda v2 = new Venda(2,"Licenca de Software", 130.00M, dataAtual);
 
 listaVendas.Add(v1);
 listaVendas.Add(v2);
