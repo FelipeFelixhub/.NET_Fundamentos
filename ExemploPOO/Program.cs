7b9f1d1ea20237fbbf17073a51ff580387444aa3
Felipe Felix <felipe.dfelix@gmail.com>
2023-07-18 18:39:05 -0300

    Heranca e Polimorfismo

--- ExemploPOO/Program.cs
+++ ExemploPOO/Program.cs
@@ -1,10 +1,33 @@
 ﻿using ExemploPOO.Models;
 
-ContaCorrente c1 = new ContaCorrente(123, 1000);
 
-c1.ExibirSaldo();
-c1.Sacar(200);
-c1.ExibirSaldo();
+Aluno a1 = new Aluno();
+a1.Nome = "Felipe";
+a1.Idade = 31;
+a1.Nota = 10;
+a1.Apresentar();
+
+
+Professor p1 = new Professor();
+p1.Nome = "Humberto";
+p1.Idade = 45;
+p1.Salario = 15000;
+p1.Apresentar();
+
+
+
+
+
+
+
+
+
+
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+// c1.ExibirSaldo();
+// c1.Sacar(200);
+// c1.ExibirSaldo();
 
 
 
