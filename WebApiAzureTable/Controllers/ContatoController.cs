6401ff2efd7f4ed33fbb4f8adb705395bdf92e81
Felipe Felix <felipe.dfelix@gmail.com>
2023-08-04 19:39:53 -0300

    feat: final project api

--- WebApiAzureTable/Controllers/ContatoController.cs
+++ WebApiAzureTable/Controllers/ContatoController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Data.Tables;
 using Microsoft.AspNetCore.Mvc;
+using WebApiAzureTable.Models;
 
 namespace WebApiAzureTable.Controllers
 {
@@ -17,7 +19,69 @@ namespace WebApiAzureTable.Controllers
         {
             _connectionString = configuration.GetValue<string>("SAConnectionString");
             _tableName = configuration.GetValue<string>("AzureTableName");
+        }
 
+        private TableClient GetTableClient()
+        {
+            var serviceClient = new TableServiceClient(_connectionString);
+            var tableClient = serviceClient.GetTableClient(_tableName);
+
+            tableClient.CreateIfNotExists();
+            return tableClient;
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Contato contato)
+        {
+            var tableClient = GetTableClient();
+
+            contato.RowKey = Guid.NewGuid().ToString();
+            contato.PartitionKey = contato.RowKey;
+
+            tableClient.UpsertEntity(contato);
+
+            return Ok(contato);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(string id, Contato contato)
+        {
+            var tableClient = GetTableClient();
+            var contatoTable = tableClient.GetEntity<Contato>(id, id).Value;
+
+            contatoTable.Nome = contato.Nome;
+            contatoTable.Email = contato.Email;
+            contatoTable.Telefone = contato.Telefone;
+
+            tableClient.UpsertEntity(contatoTable);
+            return Ok();
+        }
+
+        [HttpGet("Listar")]
+        public IActionResult ObterTodos()
+        {
+            var tableClient = GetTableClient();
+            var contatos = tableClient.Query<Contato>().ToList();
+            
+            return Ok(contatos);
+        }
+
+        [HttpGet("Listar/{nome}")]
+        public IActionResult ObterPorNome(string nome)
+        {
+            var tableClient = GetTableClient();
+            var contatos = tableClient.Query<Contato>(x => x.Nome == nome).ToList();
+            
+            return Ok(contatos);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(string id)
+        {
+            var tableClient = GetTableClient();
+            tableClient.DeleteEntity(id, id);
+
+            return NoContent();
         }
 
     }
