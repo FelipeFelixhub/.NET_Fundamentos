4d16bbd9c5802de8119917ccd568ed392ce180a1
Felipe Felix <felipe.dfelix@gmail.com>
2023-08-04 14:59:30 -0300

    feat: adding DTO Blob

---
+++ AzureStorageAPI/BlobDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureStorageAPI
+{
+    public class BlobDto
+    {
+        public string Nome { get; set; }
+        public string tipo { get; set; }    
+        public string Uri { get; set; }
+    }
+}
