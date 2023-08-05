22ce98dbc6186997c3766ecdc6b230be6e7ca52d
Felipe Felix <felipe.dfelix@gmail.com>
2023-08-04 22:29:09 -0300

    feat: adding Calculadora class

--- ModuloTeste/Calculadora/Program.cs
+++ ModuloTeste/Calculadora/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Calculadora.Service;
+
+CalculadoraImp c = new CalculadoraImp();
+
+int num1 = 5;
+int num2 = 15;
+
+System.Console.WriteLine($"{num1} + {num2} = {c.Somar(num1, num2)}");
