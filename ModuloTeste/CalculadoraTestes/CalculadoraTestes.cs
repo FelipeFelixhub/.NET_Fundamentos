85888e40bd870e8e7fb574d340be77bf84c89b48
Felipe Felix <felipe.dfelix@gmail.com>
2023-08-07 14:59:33 -0300

    feat: String Validation

--- ModuloTeste/CalculadoraTestes/CalculadoraTestes.cs
+++ ModuloTeste/CalculadoraTestes/CalculadoraTestes.cs
@@ -10,7 +10,7 @@ public class CalculadoraTestes
     {
         _calc = new CalculadoraImp();
     }
-    
+
     [Fact]
     public void DeveSomar5Com10eRetornar15()
     {
@@ -23,5 +23,19 @@ public class CalculadoraTestes
 
         // Assert
         Assert.Equal(15, resutado);
+    }
+
+     [Fact]
+    public void DeveSomar10Com10eRetornar20()
+    {
+        // Arrange
+        int num1 = 10;
+        int num2 = 10;
+
+        // Act
+        int resutado = _calc.Somar(num1, num2);
+
+        // Assert
+        Assert.Equal(20, resutado);
     }
 }
